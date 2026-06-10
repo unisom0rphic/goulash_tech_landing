51badfda5a850bd8df430fa681e08cdf3436f1ba
unisom0rphic <117302827+unisom0rphic@users.noreply.github.com>
2026-06-10 15:50:20 +0500

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit({
+			adapter: adapter({
+				fallback: '404.html',
+			}),
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			}
+		}), 
+	],
+
 });
